27d5819d541cc1a6be457c0e9f16f0cf69108aaa
Keaton Proctor <proctor.keaton@gmail.com>
2023-06-24 23:51:29 -0600

    Create UVSimRunner.java

---
+++ GroupHProject/src/UVSimRunner.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class UVSimRunner {
+	
+	public static void main(String[] args) {
+		
+		FileReaderGUI simGUI = new FileReaderGUI();
+		
+		simGUI.main(args);
+				
+
+		
+		
+	}
+
+}
