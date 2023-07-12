406a0b3513bfdcce9e4ab1a4e2c6a4f5465da201
Keaton Proctor <proctor.keaton@gmail.com>
2023-07-11 22:13:53 -0600

    Update UVSimRunner.java
    Made few changes and cleaned up static calls not being called in a static way.

--- GroupHProject/src/UVSimRunner.java
+++ GroupHProject/src/UVSimRunner.java
@@ -1,15 +1,8 @@
-import java.io.File;
-
 public class UVSimRunner {
 	
 	public static void main(String[] args) {
 		
-		FileReaderGUI simGUI = new FileReaderGUI();
-		
-		simGUI.main(args);
-				
-
-		
+		FileReaderGUI.main(args);
 		
 	}
 
