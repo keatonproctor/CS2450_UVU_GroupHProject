7a0ee66bf1953009d09225363f4679430e47b2b8
Keaton Proctor <proctor.keaton@gmail.com>
2023-07-12 22:08:17 -0600

    Changed the UVSim_Runner.java
    Renamed UVSim_Runner.java to be RunFileFromGUI.java

---
+++ GroupHProject/src/RunFileFromGUI.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+public class RunFileFromGUI {
+
+    public static void main(String[] args) {
+        UVSim simInstance = new UVSim();
+        FileReaderGUI guiWindow = new FileReaderGUI();
+        FileReaderGUI.runGUI();
+
+        while (guiWindow.isFileNotInitialized()) {
+            // Wait until the file is selected in the GUI
+        }
+
+        File programFile = guiWindow.getProgramFile();
+        try {
+            int[] program = simInstance.readProgramFromFile(programFile);
+            simInstance.loadProgram(program);
+
+            // Pass the UVSim instance to the GUI for command modification
+            guiWindow.setSimulator(simInstance);
+            guiWindow.displayCommandsFromFile(programFile);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
