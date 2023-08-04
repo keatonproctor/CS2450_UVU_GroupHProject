432f40fc67fbd290a6d9de8ce80e77a6ff1283b9
Keaton Proctor <proctor.keaton@gmail.com>
2023-08-03 23:16:12 -0600

    Update RunFileFromGUI.java
    Made a few changes to get rid of invalid opcode error.

--- GroupHProject/src/RunFileFromGUI.java
+++ GroupHProject/src/RunFileFromGUI.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class RunFileFromGUI {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         UVSim simInstance = new UVSim();
         FileReaderGUI guiWindow = new FileReaderGUI();
         guiWindow.setSimulator(simInstance); // Set the simulator instance
@@ -15,14 +15,13 @@ public class RunFileFromGUI {
         File programFile = guiWindow.getProgramFile();
         try {
             int[] program = simInstance.readProgramFromFile(programFile);
-            simInstance.loadProgram(program);
-
-            // Pass the UVSim instance to the GUI for command modification
-            guiWindow.setSimulator(simInstance);
             guiWindow.displayCommandsFromFile(programFile);
-
+            simInstance.loadProgram(program);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // Once the program is loaded, run it
+        simInstance.runProgram();
     }
 }
